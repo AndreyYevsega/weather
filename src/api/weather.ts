ebb8de067d6068ac0841db280242ce6173342425
AndreyYevsega <andrii.yevsega@gmail.com>
2020-08-05 16:59:38 +0300

    hide secret key

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -4,9 +4,7 @@ export const getWeather = async <DataType>() => {
   const proxy: string = process.env.REACT_APP_PROXY_URL as string;
   const secret: string = process.env.REACT_APP_SECRET_KEY as string;
 
-  const response = await fetch(
-    `${proxy}/https://api.weatherbit.io/v2.0/forecast/daily?city=Dnipro&key=3883f69ab25f46dc92cdf2b5d66a1517`
-  );
+  const response = await fetch(`${proxy}/https://api.weatherbit.io/v2.0/forecast/daily?city=Dnipro&key=${secret}`);
 
   return await formalizeReturn<DataType>(response);
 };
